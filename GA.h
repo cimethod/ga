3f88dc16efb79385234eb5b4cc9b00a991c17e6d
shahimian <shahimian@gmail.com>
2019-01-15 16:50:33 +0330

    add manipulation chromosome header

--- GA.h
+++ GA.h
@@ -8,11 +8,11 @@
 template <class T>
 class __declspec(dllexport) GA
 {
-    Matrix 
     double (*pf);
-
+    std::string _init;
+    int _position;
     public:
-    GA(double[][2], double (*)(vector<T>), int, int, double, double);
+    GA(double[][2], double (*)(vector<T>), int, int, double, double, string, int);
     ~GA(void);
     void init(std::string &c);
     int random(std::string c);
@@ -23,4 +23,7 @@ class __declspec(dllexport) GA
     int countCromosome(double c[], int radix, int n);
     long int bindec(std::string c);
     double binreal(std::string c, double a, double b);
+    string manipulationChromosome(string);
+    void init(string);
+    void position(int);
 };
